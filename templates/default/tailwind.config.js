2624942145a908c75a912f1bd60d0fd53a85adab
Lucas Yang <yangchenshin77@gmail.com>
2021-12-28 21:27:13 +0800

    Update to Tailwind CSS 3.0

--- templates/default/tailwind.config.js
+++ templates/default/tailwind.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
+  content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
-  darkMode: false,
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
